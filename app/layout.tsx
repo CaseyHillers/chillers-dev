cfd85b77bf52f5964207d3be388ebe197d29eabc
Casey Hillers <caseyhillers@gmail.com>
2026-02-05 21:21:02 -0800

    simplify description

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ const inter = Inter({
 
 export const metadata: Metadata = {
   title: "Casey Hillers",
-  description: "Personal blog and portfolio",
+  description: "Personal blog",
   manifest: "/site.webmanifest",
 };
 
